ec6d2d4a477c8076ccf02e35f4a79a316b52795f
Ikuya Nagaki <ikuya313198@gmail.com>
2023-10-10 14:58:29 +0900

    add backup function

--- src/main.py
+++ src/main.py
@@ -2,15 +2,29 @@ import sys
 
 from aspect.aspect_parser import AspectParser
 from base.c_parser import CParser
-from util.file_util import generate_full_path
+from util.file_util import backup_file
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         filename = sys.argv[1]
     else:
-        aspect_parser = AspectParser("/acc/aspect.acc")
+        # aspect_parser = AspectParser("/acc/aspect.acc")
         # aspects = aspect_parser.parse()
 
-        base_filename = CParser("/acc/base.c")
+        # base_filename = CParser("/acc/base.c")
         # base_filename = CParser("/toppers/kernel/alarm.c")
-        base_filename.parse()
+        # base_filename.parse()
+
+        backup_file("/acc/foo.b")
+
+        # base_src = open(base_src_path, "r")
+        # line_cnt = 0
+        # for line in base_src:
+        #     print(line, end="")
+        #     line_cnt += 1
+        # base_src.close()
+
+        # weaving_src_path = generate_full_path("/acc/base.c")
+        # # TODO ↑ファイル名を定数にしたい
+        # with open(weaving_src_path, "x") as f:
+        #     print("hoge", file=f)
--- src/util/file_util.py
+++ src/util/file_util.py
@@ -1,13 +1,24 @@
 from pathlib import Path
+from shutil import copy2
 
 
 def generate_full_path(path):
     """絶対パスを生成
 
     Arg:
-      path (str): Lark/からの相対パス
+      path (str): プロジェクトのルートからの相対パス
 
     Return:
       str: 引数pathへの絶対パス
     """
     return str(Path(__file__).parents[2]) + path
+
+
+def backup_file(path):
+    """バックアップファイルを作成
+    Args:
+      path (str): プロジェクトのルートからの相対パス
+    """
+    target_path = generate_full_path(path)
+    backup_path = target_path + ".bak"
+    copy2(target_path, backup_path)
