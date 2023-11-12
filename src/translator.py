6ca87ca4d2e1acd4ce65545c3db534e7223d3208
Ikuya Nagaki <ikuya313198@gmail.com>
2023-11-12 14:28:20 +0900

    refactor: make translator class

--- src/translator.py
+++ src/translator.py
@@ -5,29 +5,38 @@ from customizer.lang_processor.aspect_parser import AspectParser
 from customizer.src import Src
 from util.file_util import backup_file, generate_full_path
 
+
+class Translator:
+    def __init__(self):
+        if len(sys.argv) > 1:
+            self.aspect_file = sys.argv[1]
+            self.base_file = sys.argv[2]
+        else:
+            self.aspect_file = "acc/aspect.acc"
+            self.base_file = "acc/base.c"
+
+    def parse_aspect(self):
+        aspects = (AspectParser(self.aspect_file)).parse()
+        return aspects
+
+    def parse_base(self):
+        c_ast = CParser(self.base_file).parse()
+        return c_ast
+
+    def translate(self, aspects, c_ast):
+        backup_file(self.base_file)
+        target_path = generate_full_path(self.base_file)
+        with open(target_path, mode="r") as f:
+            target_src = f.readlines()
+        customized_src = Src(target_src)
+        for asp in aspects:
+            asp.weave(customized_src, c_ast)
+        with open(target_path, mode="w") as f:
+            f.writelines(customized_src.get())
+
+
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        aspect_file = sys.argv[1]
-        base_file = sys.argv[2]
-    else:
-        aspect_file = "acc/aspect.acc"
-        base_file = "acc/base.c"
-
-    """Parse aspect file"""
-    aspects = (AspectParser(aspect_file)).parse()
-
-    """Parse source file for customization"""
-    c_parser = CParser(base_file)
-    # c_parser = CParser("toppers/kernel/alarm.c")
-    c_ast = c_parser.parse()
-
-    """Execute customizations"""
-    backup_file(base_file)
-    target_path = generate_full_path(base_file)
-    with open(target_path, mode="r") as f:
-        target_src = f.readlines()
-    customized_src = Src(target_src)
-    for asp in aspects:
-        asp.weave(customized_src, c_ast)
-    with open(target_path, mode="w") as f:
-        f.writelines(customized_src.get())
+    translator = Translator()
+    aspects = translator.parse_aspect()
+    c_ast = translator.parse_base()
+    translator.translate(aspects, c_ast)
