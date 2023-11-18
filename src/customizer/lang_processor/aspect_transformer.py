627c1e2a529e04a358ce7f9abe623b9320b3f8dd
Ikuya Nagaki <ikuya313198@gmail.com>
2023-11-18 17:23:22 +0900

    refactor: remove abstract_aspect and concrete_aspect from aspect_transformer

--- src/customizer/lang_processor/aspect_transformer.py
+++ src/customizer/lang_processor/aspect_transformer.py
@@ -2,12 +2,7 @@ from lark import Token, Transformer
 
 from customizer.aspect.advice import Advice
 from customizer.aspect.aspect import Aspect
-from customizer.aspect_container.abstract_aspect import AbstractAspect
 from customizer.aspect_container.basic_aspect import BasicAspect
-from customizer.aspect_container.concrete_aspect import ConcreteAspect
-from customizer.aspect_container.constructor import Constructor
-from customizer.aspect_container.super import Super
-from customizer.method.abstract_method import AbstractMethod
 from customizer.method.method import Method
 from customizer.pointcut.execution import Execution
 from customizer.pointcut.func_signature import FuncSignature
@@ -18,35 +13,6 @@ class AspectTransformer(Transformer):
         return tree
 
     ######################## aspect ########################
-    def aspect_container(self, tree):
-        return tree[0]
-
-    def abstract_aspect(self, tree):
-        """
-        abstract aspect Foo {
-            Aspects
-        }
-        """
-        name = str(tree[0])
-        constructor: Constructor = tree[1]
-        abstract_methods = [t for t in tree[2:] if isinstance(t, AbstractMethod)]
-        methods = [t for t in tree[2:] if isinstance(t, Method)]
-        aspects = [t for t in tree[2:] if isinstance(t, Aspect)]
-        return AbstractAspect(name, constructor, abstract_methods, methods, aspects)
-
-    def concrete_aspect(self, tree):
-        """
-        aspect Bar extends Foo {
-            Aspects
-        }
-        """
-        name = str(tree[0])
-        super_asp_name = str(tree[1])
-        super: Super = tree[2]
-        methods = [t for t in tree[3:] if isinstance(t, Method)]
-        aspects = [t for t in tree[3:] if isinstance(t, Aspect)]
-        return ConcreteAspect(name, super_asp_name, super, methods, aspects)
-
     def basic_aspect(self, tree):
         """aspect Baz {
             Aspects
@@ -92,30 +58,6 @@ class AspectTransformer(Transformer):
     def infunc(self, tree):
         return {"name": "infunc", "arg": tree[0]}
 
-    ######################## constructor ########################
-    def constructor(self, tree):
-        return Constructor(tree[1])
-
-    def constructor_args(self, tree):
-        """
-        return:
-            e.g.) ['foo', 'bar']
-        """
-        return [str(t) for t in tree]
-
-    def super(self, tree):
-        return Super(tree[1])
-
-    def super_args(self, tree):
-        """
-        return:
-            e.g.) foo.c, "bar2" →[foo.c, "bar2"]
-        """
-        return tree
-
-    def super_arg(self, tree):
-        return "".join([t.value for t in tree])
-
     ######################## method ########################
     def method(self, tree):
         """
@@ -123,12 +65,6 @@ class AspectTransformer(Transformer):
         """
         type, names, args, body = tree[0].type, tree[0].name, tree[0].args, tree[1]
         return Method(type, names, args, body)
-
-    def abstract_method(self, tree):
-        """
-        e.g.) tree[abstract, FuncSignature] → int foo(char)
-        """
-        return AbstractMethod(tree[1].type, tree[1].name, tree[1].args)
 
     ######################## func ########################
 
