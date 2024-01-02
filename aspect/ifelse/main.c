229880b7a11de64fc8e74cffd1577f8c096a6f88
Ikuya Nagaki <ikuya313198@gmail.com>
2024-01-02 16:41:38 +0900

    chore

---
+++ aspect/ifelse/main.c
@@ -0,0 +1,12 @@
+int main() {
+  { /* Start of advice*/
+    if (1) {
+      printf("Hello, world!\n");
+    } else {
+      printf("Goodbye, world!\n");
+    }
+  } /* End of advice */
+
+  printf("Hello, World!\n");
+  return 0;
+}
