3926145c52d18383e005386366782242bca5144c
Ikuya Nagaki <ikuya313198@gmail.com>
2024-01-02 16:41:11 +0900

    refactor: generate abs path

--- src/util/file_util.py
+++ src/util/file_util.py
@@ -2,16 +2,17 @@ from pathlib import Path
 from shutil import copy2
 
 
-def generate_abs_path(path):
+def generate_abs_path(path: str):
     """絶対パスを生成
 
     Arg:
       path (str): プロジェクトのルートからのパス
-      e.g. "src/util/file_util.py"
+      e.g. "/src/util/file_util.py"
 
     Return:
       str: 引数pathへの絶対パス
     """
+    path.lstrip("/")  # 補完を効かせるため"/"付きで受け取り､内部で削除
     return Path(__file__).resolve().parents[2].joinpath(path)
 
 
